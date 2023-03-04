7cfeacd155686b6512cf9d54577d0c606a3af3ef
Sonny May <102835434+sonnymay@users.noreply.github.com>
2023-03-04 15:56:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [90, 80, 50];
+const progressBarPercents = [90, 80, 60];
 
 window.addEventListener("scroll", () => {
   mainFn();
