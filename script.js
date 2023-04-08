4e7b75c2eb2510a519ed0a74c73340abb060ed06
Sonny May <102835434+sonnymay@users.noreply.github.com>
2023-04-08 11:42:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,51 @@ mainFn();
 window.addEventListener("resize", () => {
   window.location.reload();
 });
+
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+// Animated Skills Progress Bars
+function animateSkillBars() {
+  const skillBars = document.querySelectorAll('.skill-bar');
+
+  skillBars.forEach(skillBar => {
+      const skillLevel = skillBar.getAttribute('data-level');
+      skillBar.style.width = `${skillLevel}%`;
+  });
+}
+
+window.addEventListener('scroll', () => {
+  const triggerPoint = window.innerHeight / 5 * 4;
+  const skillsSection = document.querySelector('#skills');
+
+  if (skillsSection.getBoundingClientRect().top < triggerPoint) {
+      animateSkillBars();
+  }
+});
+
+// Back to Top Button
+const backToTopButton = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (document && document.
+
+    body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      backToTopButton.style.display = "block";
+  } else {
+      backToTopButton.style.display = "none";
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
